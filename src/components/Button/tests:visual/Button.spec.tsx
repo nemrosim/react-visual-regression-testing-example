28fd382149b4c56ea8fd3873696a19ee3ac441b5
nemrosim <nemrosim1988@gmail.com>
2022-02-24 04:26:30 +0200

    fixes, skip visual tests

--- src/components/Button/tests:visual/Button.spec.tsx
+++ src/components/Button/tests:visual/Button.spec.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { generateImage, setDefaultOptions } from 'jsdom-screenshot';
-import { Button, hovered } from './Button';
+import { Button, hovered } from '../Button';
 import styled from 'styled-components';
 import userEvent from '@testing-library/user-event';
-
-setDefaultOptions({
-    launch: {
-        executablePath: process.env.CI === 'true' ? '/usr/bin/chromium-browser' : undefined,
-        // TravisCI and Linux OS require --no-sandbox to be able to run the tests
-        // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
-        args: process.env.CI === 'true' ? ['--no-sandbox'] : [],
-    },
-});
 
 // give tests more time as taking screenshots takes a while
 jest.setTimeout(10000);
