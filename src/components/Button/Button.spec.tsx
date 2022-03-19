4bb5ad5c16f8b63d7a49cb9517cd3feecdfd27c5
nemrosim <nemrosim1988@gmail.com>
2022-03-19 17:05:02 +0200

    updates

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from './Button';
 
 it('should render button', () => {
     render(<Button />);
     expect(screen.getByText('Hello world')).toBeInTheDocument();
+});
+
+it('should render component onClick', () => {
+    render(<Button />);
+    fireEvent.click(screen.getByText('Hello world'));
+    expect(screen.getByText('Hello')).toBeInTheDocument();
 });
